0d49c5818bf34d37062748ec87005f7e29ac5bae
OsvaldoT1995 <105831167+OsvaldoT1995@users.noreply.github.com>
2022-05-18 21:51:28 -0400

    Add files via upload

---
+++ ArrayListExample.java
@@ -0,0 +1,39 @@
+/**
+ * @(#)ArrayListExample.java
+ *
+ * ArrayListExample application
+ *
+ * @author 
+ * @version 1.00 2021/4/29
+ */
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    
+    public static void main(String[] args) {
+    	
+    	// TODO, add your application code
+    	System.out.println("Hello World! Array list");
+    	System.out.println("Programa para manejo de la clase ArrayList");
+
+    	ArrayList<String> autos = new ArrayList<String>();
+    	autos.add("Volvo");
+    	autos.add("BMW");
+    	autos.add("Ford");
+    	autos.add("Mazda");
+    	System.out.println(autos);
+    	System.out.println("Elemento 2: " + autos.get(2));
+    	
+    	ArrayList<Integer> numeros = new ArrayList<Integer>();
+    	numeros.add(1);
+    	numeros.add(3);
+    	numeros.add(2);
+    	numeros.add(5);
+    	numeros.add(4);
+    	System.out.println(numeros);
+    	System.out.println("Tamaño de lista de números: " + numeros.size());
+    	numeros.remove(2);
+    	System.out.println(numeros);
+    	System.out.println("Tamaño de lista de números: " + numeros.size());
+    }
+}
